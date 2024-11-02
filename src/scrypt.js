dded93a99bfe62ab319f7405409f3ee9862a38a7
cleanverseca <cleanverse.ca@gmail.com>
2024-11-01 20:31:27 -0700

    map not working

---
+++ src/scrypt.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import * as L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+const map = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors,"
+}).addTo(map);
