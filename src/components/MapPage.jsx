d89494ef482ce109464dd9c0aae28cf76bb7055a
chl55 <chl55@sfu.ca>
2024-11-22 23:09:35 -0800

    Separated instructions to its own component

--- src/components/MapPage.jsx
+++ src/components/MapPage.jsx
@@ -1,4 +1,5 @@
 import Map from "./Map";
+import Instructions from "./Instructions";
 import React from "react";
 import Navbar from "./Navbar";
 import './main.css';
@@ -12,19 +13,7 @@ function MapPage(){
             </div>
             <div className="map-container">
                 <Map />
-                <p className="map-p">
-                    <h3>How to Use</h3>
-                    <ul className="map-instructions">
-                        <li>Click on a map marker to see details about the selected emergency.
-                            These details will appear in a pop-up.</li>
-                        <li>Zoom in or out on the map to view more or fewer emergency markers.</li>
-                        <li>Click on a list item below to view the emergency details. The corresponding
-                            marker on the map will also be highlighted for easy identification.</li>
-                        <li>To submit a report, right-click on the map and fill out the form
-                            accordingly. Once properly filled out, the report will be added to the map.
-                        </li>
-                    </ul>
-                </p>
+                <Instructions />
             </div>
         </div>
     );
