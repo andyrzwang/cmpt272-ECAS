a81ba92d0dcaef49e23491c1e65bed218388d54a
chl55 <chl55@sfu.ca>
2024-11-22 22:56:48 -0800

    Added right click to add marker

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -12,6 +12,20 @@ function Map (){
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
 
+        const yellowMarker = L.icon({
+          iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-gold.png',
+          iconAnchor: [12, 41],
+          popupAnchor: [0, -34],
+        });
+        
+        // Add a marker on right click
+        map.on("contextmenu", (event) => {
+        const { lat, lng } = event.latlng;
+        const marker = L.marker([lat, lng], {icon: yellowMarker}).addTo(map);
+        // Temporary popup
+        marker.bindPopup(`<b>You clicked here:</b><br>Lat: ${lat.toFixed(5)}, Lng: ${lng.toFixed(5)}`).openPopup();
+    });
+
         return () => {
             map.remove();
         };
