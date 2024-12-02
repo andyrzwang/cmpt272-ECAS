1047b571e66af63a0c23d1c6d65d3792bce0141e
anthony-gudiel <adg15@sfu.ca>
2024-12-01 20:28:39 -0800

    Display board is now sortable by status

--- src/components/emergency-list/EmergencyList.jsx
+++ src/components/emergency-list/EmergencyList.jsx
@@ -1,11 +1,41 @@
 import React from "react";
 import "./emergencyList.css";
 import { getAllReports } from "../storage/storage";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function EmergencyList() {
 
-  const reports = getAllReports();
+  const [reports, setReports] = useState([]);
+  const [isSorted, setIsSorted] = useState(false); 
+
+  useEffect(() => {
+    const storedReports = getAllReports();
+    setReports(storedReports);
+  }, []);
+
+  const toggleSort = () => {
+    const sortedReports = [...reports].sort((a, b) => {
+      if (isSorted) {
+        if (a.status === 'Closed' && b.status === 'Open') {
+          return -1;
+        } else if (a.status === 'Open' && b.status === 'Closed') {
+          return 1;
+        }
+      } else {
+        // Ascending order (open first)
+        if (a.status === 'Open' && b.status === 'Closed') {
+          return -1;
+        } else if (a.status === 'Closed' && b.status === 'Open') {
+          return 1;
+        }
+      }
+      return 0;
+    });
+
+    setReports(sortedReports);
+    setIsSorted(!isSorted);
+  };
+
   const reportsPerPage = 5;
   const [currentStartIndex, setCurrentStartIndex] = useState(0);
 
@@ -34,14 +64,20 @@ function EmergencyList() {
     }
   }
 
+
   return (
 
     <div className="emergency-list">
-      <h2 className="emergency-list-header">Emergency List</h2>
-      <h4><em>Click submission for more details</em></h4>
+      <h2 className="emergency-list-header">Emergency List <br />
+      <em className="sub-heading">Click on a submission for more details</em>
+      </h2>
       
-        <ul className="list">
-          {currentPageReports.map((report) => (
+      <button onClick={toggleSort}>
+        {isSorted ? 'Sort: Open First' : 'Sort: Closed First'}
+      </button>
+
+      <ul className="list">
+          {currentPageReports.map((report) => (  
           <li key={report.id} onClick={() => displayDetails(report.id)}>
             <div className="main-details">
               <p><strong>Type: </strong><br />{report["emergency-type"]}</p>
