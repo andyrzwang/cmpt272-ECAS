209553cd39bdfc33e5a175da462827ec9b26a8bb
anthony-gudiel <adg15@sfu.ca>
2024-12-02 13:11:11 -0800

    Added comments to EmergencyListjsx

--- src/components/emergency-list/EmergencyList.jsx
+++ src/components/emergency-list/EmergencyList.jsx
@@ -6,6 +6,7 @@ import { getIsUserLoggedIn } from "../session";
 
 function EmergencyList() {
 
+  // Initialize states for reports and IsSorted
   const [reports, setReports] = useState([]);
   const [isSorted, setIsSorted] = useState(false); 
 
@@ -16,6 +17,7 @@ function EmergencyList() {
 
   const toggleSort = () => {
     const sortedReports = [...reports].sort((a, b) => {
+      // If already sorted, sort by descending order (closed first)
       if (isSorted) {
         if (a.status === 'Closed' && b.status === 'Open') {
           return -1;
@@ -23,7 +25,7 @@ function EmergencyList() {
           return 1;
         }
       } else {
-        // Ascending order (open first)
+        // Else, ascending order (open first)
         if (a.status === 'Open' && b.status === 'Closed') {
           return -1;
         } else if (a.status === 'Closed' && b.status === 'Open') {
@@ -33,46 +35,63 @@ function EmergencyList() {
       return 0;
     });
 
+    // Update the state of reports and isSorted
     setReports(sortedReports);
     setIsSorted(!isSorted);
   };
 
   const reportsPerPage = 5;
+  // Initialize state for currentStartIndex
   const [currentStartIndex, setCurrentStartIndex] = useState(0);
-
+  // Calculate endIndex to mark the end of the reports to display on the current page
   const endIndex = Math.min(currentStartIndex + reportsPerPage, reports.length);
+  // Slice the reports array to get the reports to display on the current page
   const currentPageReports = reports.slice(currentStartIndex, endIndex);
   
   const handleNext = () => {
+    // Check if there are more reports to display on the next page
+    // If there are, update the currentStartIndex forward 5 pages
     if (currentStartIndex + reportsPerPage < reports.length) {
       setCurrentStartIndex(currentStartIndex + reportsPerPage);
     }
   };
   
   const handlePrevious = () => {
+    // Check if there are any reports to display on the previous page
+    // If there are, update the currentStartIndex back 5 reports
     if (currentStartIndex - reportsPerPage >= 0) {
       setCurrentStartIndex(currentStartIndex - reportsPerPage);
     }
   };
 
+  // Initialize state for expandedReportId
   const [expandedReportId, setExpandedReportId] = useState(null);
 
+  // Display details of corresponding report (based on ID) when clicked
   const displayDetails = (reportId) => {
+    // If the report is already expanded, collapse it
     if (expandedReportId === reportId) {
       setExpandedReportId(null);
+    // Else, expand the report
     } else {
       setExpandedReportId(reportId);
     }
   }
 
   const deleteReport = (reportId) => {
+    // Retrieve the user's login status
     const userStatus = getIsUserLoggedIn();
+    // If the user is not logged in, prompot the user to log in and return
     if (!userStatus) {
       alert("You must be logged in to delete a submission");
       return;
+    // If the user is logged in, delete the report
     } else{
+      // Filter out the report to be deleted
       const updatedReports = reports.filter((report) => report.id !== reportId);
+      // Remove the report from storage
       localStorage.removeItem(reportId);
+      // Update the state of reports
       setReports(updatedReports);
     }
   };
